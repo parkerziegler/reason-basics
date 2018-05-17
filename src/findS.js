01c56505bae9d8a3a1641bf486dc31b12cf78905
parkerziegler <parker.ziegler@formidable.com>
2018-05-17 10:26:36 -0700

    Add more examples to external JS objects.

--- src/findS.js
+++ src/findS.js
@@ -2,5 +2,10 @@ module.exports = {
   findS(str) {
     const sCount = str.split('').filter(char => char === 's');
     return sCount.length;
+  },
+  myJSObject: {
+    language: "JavaScript",
+    users: 5000000,
+    color: "yellow"
   }
 };
